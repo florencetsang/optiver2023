353931770b38a900b16b102437bc6fc54b5e9585
Florence <florencesytsang@gmail.com>
2024-01-08 23:56:45 +0800

    Dummy baseline models.

---
+++ florence/model_pipeline/dummy_models.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.base import BaseEstimator
+
+class BaselineEstimator(BaseEstimator):
+    def predict(self, X):
+        return np.zeros(len(X))
+    
+class SimpleEstimator(BaseEstimator):
+
+    simple_mapping = {
+        1: 0.1,
+        0: 0,
+        -1: -0.1
+    }
+
+    def predict(self, X):
+        return X['imbalance_buy_sell_flag'].map(self.simple_mapping)
+
