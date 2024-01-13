13fcb1820b53df3b4b30090c16a707e65272bb72
Florence <florencesytsang@gmail.com>
2024-01-13 17:17:05 +0800

    Remove commented out lines.

--- florence/data_preprocessor/feature_engineering.py
+++ florence/data_preprocessor/feature_engineering.py
@@ -34,13 +34,6 @@ class EnrichDFDataPreprocessor(DataPreprocessor):
         # From Andy - Pressure & Inefficiency
         df_['pressure'] = self.calculate_pressure(df)
         df_['inefficiency'] = df.eval('imbalance_size/matched_size')
-  
-        # Rolling based features
-         # df_['wap_30'] = df.groupby(['stock_id', 'date_id'])['wap'].rolling(window=3,min_periods=3).mean()  
-        # df_['wap_30'] = df.groupby(['stock_id', 'date_id'])['wap'].transform(lambda s: s.rolling(3, min_periods=1).mean())
-        # df_['wap_60'] = df.groupby(['stock_id', 'date_id'])['wap'].transform(lambda s: s.rolling(6, min_periods=3).mean())
-        # df_['wap_120'] = df.groupby(['stock_id', 'date_id'])['wap'].transform(lambda s: s.rolling(12, min_periods=6).mean())
-        # df_['wap_240'] = df.groupby(['stock_id', 'date_id'])['wap'].transform(lambda s: s.rolling(24, min_periods=12).mean())
 
         return df_
 
@@ -48,33 +41,11 @@ class MovingAvgPreProcessor(DataPreprocessor):
     def __init__(self, feature_name):
         super().__init__()
         self.feature_name = feature_name
-
-    # def _get_col_name(self, window, min_periods):
-    #     return f"mov_avg_{window}_{min_periods}"  # e.g. mov_avg_3_1
-    
-    # def _get_orig_idx_col_name(self, window, min_periods):
-    #     col_name = self._get_col_name(window, min_periods)
-    #     return f"orig_df_idx_{col_name}"  # e.g. orig_df_idx_mov_avg_3_1
     
     def _get_mov_avg_col_name(self, window, min_periods):
         # col_name = self._get_col_name(window, min_periods)
         return f"{self.feature_name}_mov_avg_{window}_{min_periods}"  # e.g. wap_mov_avg_3_1
 
-    # def _calc_mov_avg(self, grouped_df, consolidated_mov_avg_df, window, min_periods):
-    #     orig_idx_col_name = self._get_orig_idx_col_name(window, min_periods)
-    #     mov_avg_col_name = self._get_mov_avg_col_name(window, min_periods)
-
-    #     mov_avg_df = grouped_df[self.feature_name].rolling(window=window, min_periods=min_periods).mean()
-    #     consolidated_mov_avg_df[orig_idx_col_name] = mov_avg_df.index
-    #     consolidated_mov_avg_df[mov_avg_col_name] = mov_avg_df[self.feature_name]
-
-    #     return consolidated_mov_avg_df
-    
-    # def _enrich_orig_df(self, orig_df, consolidated_mov_avg_df, window, min_periods):
-    #     mov_avg_col_name = self._get_mov_avg_col_name(window, min_periods)
-    #     orig_df[mov_avg_col_name] = consolidated_mov_avg_df[mov_avg_col_name]
-    #     return orig_df
-    
     def _calc_mov_avg2(self, orig_df, grouped_df, window, min_periods):
         mov_avg_col_name = self._get_mov_avg_col_name(window, min_periods)
         mov_avg_df = grouped_df[self.feature_name].rolling(window=window, min_periods=min_periods).mean()
@@ -87,20 +58,10 @@ class MovingAvgPreProcessor(DataPreprocessor):
         # sort=False --> Sort group keys. Get better performance by turning this off.
         # consolidated_mov_avg_df = pd.DataFrame()
         grouped_df = df.groupby(['stock_id', 'date_id'], as_index=False, sort=False)
-
         df = self._calc_mov_avg2(df, grouped_df, 3, 1)
         df = self._calc_mov_avg2(df, grouped_df, 6, 3)
         df = self._calc_mov_avg2(df, grouped_df, 12, 6)
         df = self._calc_mov_avg2(df, grouped_df, 24, 12)
-
-        # consolidated_mov_avg_df = self._calc_mov_avg(grouped_df, consolidated_mov_avg_df, 3, 1)
-        # consolidated_mov_avg_df = self._calc_mov_avg(grouped_df, consolidated_mov_avg_df, 6, 3)
-
-        # consolidated_mov_avg_df = consolidated_mov_avg_df.sort_values(by=["orig_df_idx_mov_avg_3_1"])
-
-        # df = self._enrich_orig_df(df, consolidated_mov_avg_df, 3, 1)
-        # df = self._enrich_orig_df(df, consolidated_mov_avg_df, 6, 3)
-        
         return df
 
 class RemoveIrrelevantFeaturesDataPreprocessor(DataPreprocessor):
