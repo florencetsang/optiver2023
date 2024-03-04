1314526e2e97ca1ccd24b2b1938a1b98b2101104
cchk331 <69382404+cchk331@users.noreply.github.com>
2024-03-04 23:07:28 +0800

    Add data manipulation method to DTWKMeansPreprocessor

--- florence/data_preprocessor/feature_engineering.py
+++ florence/data_preprocessor/feature_engineering.py
@@ -105,12 +105,17 @@ class DTWKMeansPreprocessor(DataPreprocessor):
             model.fit(time_series_data)
             inertias.append(model.inertia_)
         return inertias
-        # plt.figure(figsize=(8, 4))
-        # plt.plot(range(1, max_clusters + 1), inertias, marker='o')
-        # plt.xlabel('Number of clusters')
-        # plt.ylabel('Inertia')
-        # plt.title('Elbow Method For Optimal k')
-        # plt.show()
+    def data_manipulation(self, df):
+        df.set_index(['date_id', 'time_id', 'stock_id'], inplace=True)
+        df_unstacked = df.unstack(level='stock_id')
+        df_unstacked.columns = ['_'.join(map(str, col)).strip() for col in df_unstacked.columns.values]
+        pivoted_df = df_unstacked.fillna(0)
+        pivoted_df .reset_index(inplace=True)
+        relevant_columns = [col for col in pivoted_df.columns if col.startswith(self.target_col_name)]
+        relevant_columns =relevant_columns
+        pivoted_df = pivoted_df[relevant_columns]
+        time_series_data = pivoted_df.to_numpy()
+        return time_series_data
 
     def apply(self, df):
         print("DTWKMeansPreprocessor_start")
